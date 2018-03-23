9666ee5a1d95248fb7f5012555fa46aaa6e56cfd
Siddarth Sharma <sishar@microsoft.com>
2018-03-23 16:19:45 +0000

    feat: setup todo api

---
+++ TodoService/Repositories/TodoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoService.Models;
+
+namespace TodoService.Repositories
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
